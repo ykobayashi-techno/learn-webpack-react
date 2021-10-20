215410ac8002d2b433993f7686c1cb33d201e8e3
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-10-20 17:23:27 +0900

    簡単な外部モジュール作成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import _ from "lodash";
+import { printConsoleMessage } from "./print";
 
 function component() {
-  const element = document.createElement("div");
-
+  const content = document.createElement("div");
+  const message = document.createElement("div");
+  const button = document.createElement("button");
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  message.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  return element;
+  button.innerHTML = "Click Button";
+  button.onclick = printConsoleMessage;
+
+  content.appendChild(message);
+  content.appendChild(button);
+
+  return content;
 }
 
 document.body.appendChild(component());
