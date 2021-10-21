f6fd6469f3607a4343ec9db5540d14e9bf70e12a
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-10-22 04:17:58 +0900

    modulesの対象にsrcフォルダを追加

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,9 @@ module.exports = {
     ],
   },
 
-  // import 文で .ts ファイルを解決するため
-  // これを定義しないと import 文で拡張子を書く必要が生まれる。
-  // フロントエンドの開発では拡張子を省略することが多いので、
-  // 記載したほうがトラブルに巻き込まれにくい。
   resolve: {
-    // 拡張子を配列で指定
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    // 拡張子を配列で指定。import時拡張子を省略できる
     extensions: [".ts", ".tsx", ".js", ".json"],
   },
 
