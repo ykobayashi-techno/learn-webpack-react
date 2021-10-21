c55e7fc93628c777e4694689f0c7a310e4343f0d
ykobayashi-techno <ykobayashi@it-nasu.com>
2021-10-21 18:29:29 +0900

    Webpackの設定変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  entry: "./src/index.ts",
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader",
+      },
+    ],
+  },
+
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  // フロントエンドの開発では拡張子を省略することが多いので、
+  // 記載したほうがトラブルに巻き込まれにくい。
+  resolve: {
+    // 拡張子を配列で指定
+    extensions: [".ts", ".js"],
+  },
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
